893afc7010c5094fae43ba172576e3aff4ad87de
James Collins <james@om4.com.au>
2014-05-02 10:56:14 +0800

    Revisions documentation.

--- inc/OM4_Revisions.php
+++ inc/OM4_Revisions.php
@@ -2,13 +2,15 @@
 
 /**
  * Automatically overrides WordPress' revisions limit to 5 per post/page.
+ *
+ * Note: this overrides the WP_POST_REVISIONS constant, even if it is set to false
  */
 class OM4_Revisions extends OM4_Plugin_Base {
 
 	private $number_of_revisions_to_keep = 5;
 
 	public function plugins_loaded() {
-		// Let other plugins/themes override the default
+		// Let other plugins/themes override our default
 		$this->number_of_revisions_to_keep = apply_filters( 'om4_revisions_to_keep', $this->number_of_revisions_to_keep );
 
 		$this->filter( 'wp_revisions_to_keep' );
