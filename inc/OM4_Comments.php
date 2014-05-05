004873bb17ff11b83c1673890797e5bba7c08b93
James Collins <james@om4.com.au>
2014-05-05 14:39:06 +0800

    A more generous check of alt tag "emptiness"

--- inc/OM4_Comments.php
+++ inc/OM4_Comments.php
@@ -11,7 +11,7 @@ class OM4_Comments extends OM4_Plugin_Base {
 
 	public function get_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
 
-		if ( empty( $alt ) ) {
+		if ( empty( $alt ) || false == $alt || '' == $alt ) {
 			$author = get_comment_author( $id_or_email );
 			$alt = esc_attr( sprintf( __( 'Avatar for %s', 'om4-service' ), $author ) );
 			$avatar = str_replace( "alt=''", "alt='$alt'", $avatar );
