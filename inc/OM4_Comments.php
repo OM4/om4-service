d03cbf86a2ed062e76b711293d20d2c2d212726c
James Collins <james@om4.com.au>
2014-05-05 14:46:03 +0800

    Support user id lookup (not just comment lookup)
    Fixes Bugz 2626

--- inc/OM4_Comments.php
+++ inc/OM4_Comments.php
@@ -12,7 +12,18 @@ class OM4_Comments extends OM4_Plugin_Base {
 	public function get_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
 
 		if ( empty( $alt ) || false == $alt || '' == $alt ) {
-			$author = get_comment_author( $id_or_email );
+			$author = '';
+
+			if ( is_numeric( $id_or_email ) ) {
+				$user = get_userdata( $id_or_email );
+				if ( $user && isset( $user->display_name ) ) {
+					$author = $user->display_name;
+				}
+			}
+			if ( empty($author) ) {
+				$author = get_comment_author( $id_or_email );
+			}
+
 			$alt = esc_attr( sprintf( __( 'Avatar for %s', 'om4-service' ), $author ) );
 			$avatar = str_replace( "alt=''", "alt='$alt'", $avatar );
 		}
