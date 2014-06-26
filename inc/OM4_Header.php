cf8d641bd05df5e73679771ca39dc7db698c10f0
James Collins <james@om4.com.au>
2014-06-26 14:20:47 +0800

    Disable the rel=prev and rel=next links in <head> section

---
+++ inc/OM4_Header.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/**
+ * Header (<head>) common modifications
+ */
+class OM4_Header extends OM4_Plugin_Base {
+
+	public function plugins_loaded() {
+
+		/*
+		 * Disable the rel=prev and rel=next links in <head> section.
+		 * Otherwise hidden/private/admin pages can be found easily.
+		 * (This is already done for sites using Yoast's SEO plugin, but it should be done on all sites regardless).
+		 */
+		remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head' );
+	}
+
+}
